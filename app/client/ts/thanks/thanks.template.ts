5cf09301f2f0468f1b9d446220015e4f0ce55d93
Quique Fdez Guerra <quique@ckgrafico.com>
2017-04-03 21:46:29 +0200

    🌏 Add links

--- app/client/ts/thanks/thanks.template.ts
+++ app/client/ts/thanks/thanks.template.ts
@@ -3,7 +3,8 @@ export default options => `
         <span class="wt-home-background background-center-bottom"></span>
         <div class="wt-home-wrap wrap">
             <h1><strong>THANKS</strong></h1>
-            <h2>Quique Fdez Guerra - @CKGrafico</h2>
+            <h2>Quique Fdez Guerra - <a href="http://twitter.com/ckgrafico" target="_blank">@CKGrafico</a></h2>
+            <h4>Live slides <a href="http://bit.ly/ck-whyts" target="_blank">bit.ly/ck-whyts</a> · Github <a href="https://github.com/CKGrafico/Why-TypeScript" target="_blank">CKGrafico/Why-TypeScript</a></h4>
         </div>
 
         <img class="wt-home-plane" src="assets/images/home_plane.png">
